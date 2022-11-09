b1bea4ce792dfa333f1fe12658baee764785d8a3
Silvester Edi <edireskito@gmail.com>
2022-11-09 23:40:14 +0700

    first commit

---
+++ src/com/toko/bunga/dao/TokoBungaDao.java
@@ -0,0 +1,17 @@
+
+package com.toko.bunga.dao;
+
+import com.toko.bunga.model.TokoBunga;
+import java.util.List;
+
+/**
+ *
+ * @author Moxieee
+ */
+public interface TokoBungaDao {
+    public void save(TokoBunga tokobunga);
+    public void update(TokoBunga tokobunga);
+    public void delete(TokoBunga tokobunga);
+    public TokoBunga get(String kd_bunga);
+    public List<TokoBunga> getList();
+}
